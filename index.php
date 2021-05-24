a6fd134d117afe10c4e23467bc5b333240755425
OndrejCabyCabelka <79140324+OndrejCabyCabelka@users.noreply.github.com>
2021-05-24 11:32:36 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$gender2 = "woman";
+$gender = "man";
+if ($gender == "male");
+elseif ($gender2 =="female");
+ {echo "If you choose $gender = You are a $gender."  ;}
+
+ {echo            "If you choose $gender2 = You are a $gender2 ";}
+ 
+ ?>
